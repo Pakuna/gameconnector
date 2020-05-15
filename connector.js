110ba5eec85378704ee4935ed8068c48cef823cb
Stefan Hahn <pakuna@gmx.de>
2020-05-15 18:20:39 +0100

    JS comment

--- connector.js
+++ connector.js
@@ -1,3 +1,4 @@
+// Takes on a firebase object and handles all the connection and state stuff for you :)
 export default class GameConnector {
     constructor(firebase) {
         console.log(firebase)
