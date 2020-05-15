86caba4f5ec9347ec5f0dd14d0e22b3ba3269099
Stefan Hahn <pakuna@gmx.de>
2020-05-15 18:19:51 +0100

    Simplify

--- connector.js
+++ connector.js
@@ -1,7 +1,3 @@
-import * as firebase from "firebase/app"
-import "firebase/auth"
-import "firebase/firestore"
-
 export default class GameConnector {
     constructor(firebase) {
         console.log(firebase)
